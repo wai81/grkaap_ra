3dfb5e1bfa2e64e7096aa543824be85e1418e64f
wai81 <50743966+wai81@users.noreply.github.com>
2023-02-04 21:54:59 +0300

    change logo

--- src/components/layout/title/index.tsx
+++ src/components/layout/title/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { useRouterContext, TitleProps } from "@pankod/refine-core";
-import { Button } from "@pankod/refine-mui";
+import React from 'react';
+import { useRouterContext, TitleProps } from '@pankod/refine-core';
+import { Button } from '@pankod/refine-mui';
 
 export const Title: React.FC<TitleProps> = ({ collapsed }) => {
   const { Link } = useRouterContext();
@@ -9,15 +9,19 @@ export const Title: React.FC<TitleProps> = ({ collapsed }) => {
     <Button fullWidth variant="text" disableRipple>
       <Link to="/">
         {collapsed ? (
-          <img 
-          //src="/refine-collapsed.svg"
-          src="/grcapp-collapsed_2.svg"  
-          alt="Refine" width="28px" />
+          <img
+            //src="/refine-collapsed.svg"
+            src="/grcapp-collapsed_2.svg"
+            alt="Refine"
+            width="28px"
+          />
         ) : (
-          <img 
-          //src="/refine.svg" 
-          src="/grcapp_2.svg"  
-          alt="Refine" width="140px" />
+          <img
+            //src="/refine.svg"
+            src="/grcapp_3.svg"
+            alt="Refine"
+            width="140px"
+          />
         )}
       </Link>
     </Button>
