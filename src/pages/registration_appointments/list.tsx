d26b5708c1a4139106a7463338dbf4d2d9ad3959
wai81 <50743966+wai81@users.noreply.github.com>
2023-10-01 22:12:01 +0300

    updated RegistrationAppointmentList added data

--- src/pages/registration_appointments/list.tsx
+++ src/pages/registration_appointments/list.tsx
@@ -1,17 +1,50 @@
-import {useTranslate} from "@refinedev/core";
+import {HttpError, useList, useTranslate} from "@refinedev/core";
 import {Calendar, momentLocalizer, Event  } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
 //import 'moment/locale/ru';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import {Card, CardContent, CardHeader} from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { IBookingTransport } from "interfaces/IBookingTransport";
 
 const localizer = momentLocalizer(moment)
 
 export const RegistrationAppointmentList: React.FC = () => {
     const t = useTranslate();
+    const totalDays = 365;
+    const [today, setToday] = useState(moment())
     
+    const startDay = today.clone().startOf('year').startOf('week');
+    const startDayQuery = startDay.clone().format('YYYY-MM-DD hh:mm:ss');
+    const endDayQuery = startDay.clone().add(totalDays, 'day').format('YYYY-MM-DD hh:mm:ss')
+    const [current, setCurrent] = useState(1);
+    const [pageSize, setPageSize] = useState(100);
+
+    const { data, isLoading, isError } = useList<IBookingTransport, HttpError>({
+        resource: "booking_transport",
+        pagination: {
+            current,
+            pageSize,
+        },
+        filters: [
+            {
+                field: 'startDate',
+                operator: 'gte',
+                value: startDayQuery
+            },
+            {
+                field: 'endDate',
+                operator: 'lte',
+                value: endDayQuery
+            }
+        ]
+    });
+    
+    const eventBooking = data?.data ?? [];
+    
+    //console.log(eventBooking)
+
     const messages = {
         allDay: "Весь день",
         previous: "Назад",
@@ -28,31 +61,45 @@ export const RegistrationAppointmentList: React.FC = () => {
         showMore: (total: number) => `+ ${total} подробнее`,
     };
 
-    const myEvents = [{
-        start: '2023-10-02T08:00:00',
-        end: '2023-10-02T09:00:00',
-        allDay: false,
-        title: 'React1',
-    },
-    {
-        start: '2023-10-02T08:00:00',
-        end: '2023-10-02T09:00:00',
-        allDay: false,
-        title: 'React2',
-    },
-    {
-        start: '2023-10-02T09:30:00',
-        end: '2023-10-02T10:30:00',
-        allDay: false,
-        title: 'React3',
-    },
-    {
-        start: '2023-10-02T09:00:00',
-        end: '2023-10-02T10:00:00',
-        allDay: false,
-        title: 'React3',
-    }]
+    const myEvents = eventBooking.map((event)=>({
 
+        start: event.startDate,
+        end: event.endDate,
+        allDay: event.allDay,
+        title: `${event.title} ${event.subunit.name} ${event.transport==null? '': event.transport.title}`
+    }));
+    // const myEvents = [{
+    //     start: '2023-10-02T08:00:00',
+    //     end: '2023-10-02T09:00:00',
+    //     allDay: false,
+    //     title: 'React1',
+    // },
+    // {
+    //     start: '2023-10-02T08:00:00',
+    //     end: '2023-10-02T09:00:00',
+    //     allDay: false,
+    //     title: 'React2',
+    // },
+    // {
+    //     start: '2023-10-02T09:30:00',
+    //     end: '2023-10-02T10:30:00',
+    //     allDay: false,
+    //     title: 'React3',
+    // },
+    // {
+    //     start: '2023-10-02T09:00:00',
+    //     end: '2023-10-02T10:00:00',
+    //     allDay: false,
+    //     title: 'React3',
+    // }]
+    //console.log(myEvents)
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isError) {
+        return <div>Something went wrong!</div>;
+    }
     return(
         <Card sx={{ paddingX: { xs: 2, md: 0 } }}>
             <CardHeader title={t("filter.title")} />
